1002a2224c9878b1a1d01fd54dbbd92eb00c87d7
tim peng <typg93@gmail.com>
2021-04-09 21:12:57 -0400

    Update AIPlayer.cs

--- Assets/Scripts/AI/AIPlayer.cs
+++ Assets/Scripts/AI/AIPlayer.cs
@@ -10,24 +10,27 @@ public class AIPlayer : MonoBehaviour
 
     // ----BitBoard variables----
     // - uses bitboard data structure to store board position
-    // - flattens data in Cell Class into uint32 in series of 1's and 0's
-    // - used with bitwise operations
+    // - flattens data in Cell Class into uint32 to represent pieces locations in binary
     // - board position to bitboard uint32 digit index:
     //| 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 |
     //| 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 |
     //| 08 | 09 | 10 | 11 | 12 | 13 | 14 | 15 |
     //| 00 | 01 | 02 | 03 | 04 | 05 | 06 | 07 |
-    private uint RedOnes;
-    private uint RedTwos;
-    private uint RedThrees;
-    private uint RedFours;
-    private uint RedKing;
-    private uint BlueOnes;
-    private uint BlueTwos;
-    private uint BlueThrees;
-    private uint BlueFours;
-    private uint BlueKing;
-    private uint FaceUp;
+    private struct BitBoardValues
+    {
+        public uint RedOnes;
+        public uint RedTwos;
+        public uint RedThrees;
+        public uint RedFours;
+        public uint RedKing;
+        public uint BlueOnes;
+        public uint BlueTwos;
+        public uint BlueThrees;
+        public uint BlueFours;
+        public uint BlueKing;
+        public uint FaceUp;
+    }
+    BitBoardValues BitBoard = new BitBoardValues();
 
     private int boardX;
     private int boardY;
@@ -48,11 +51,10 @@ public class AIPlayer : MonoBehaviour
                 
             if (cellValue == 1)
             {
-                RedOnes |= SetIndexBitBoard(cellPos);
+                BitBoard.RedOnes |= SetIndexBitBoard(cellPos);
             }
         }
-        Debug.Log(Convert.ToString(RedOnes, toBase: 2));
-        LogBoardValue(RedOnes);
+        LogBoardValue(BitBoard.RedOnes);
     }
 
     uint SetIndexBitBoard(int i)
@@ -77,13 +79,11 @@ public class AIPlayer : MonoBehaviour
     }
 
     void LogBoardValue(uint bitBoard)
-        //debug and printout a bitboard value
+        //debug.log a bitboard value to match location of game board
     {
         string bitString = Convert.ToString(bitBoard, toBase: 2);
         string remainingEmptyBoard = new string('0', boardX * boardY - bitString.Length);
-
         bitString = remainingEmptyBoard + bitString;
-        Debug.Log("final bit is " + bitString);
 
         for (int i = 0; i < boardY; i++)
         {
